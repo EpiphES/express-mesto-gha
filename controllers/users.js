8ce48fdfe467da77d9b03b8ad9065c69d7a515a3
Ekaterina Epiphanova <epiph@yandex.ru>
2022-08-21 12:05:18 +0500

    feat: add getUserById route

--- controllers/users.js
+++ controllers/users.js
@@ -3,20 +3,23 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.sendStatus(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-// const getUserById = (req, res) => {
-//   const { id } = req.params;
+const getUserById = (req, res) => {
+  const { userId } = req.params;
 
-// }
+  User.find((user) => user._id === userId)
+    .then((user) => res.send({ user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.sendStatus(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports = { getUsers, createUser };
+module.exports = { getUsers, getUserById, createUser };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const userRouter = express.Router();
-const { getUsers, createUser } = require('../controllers/users');
+const { getUsers, getUserById, createUser } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
-
+userRouter.get('/:userId', getUserById);
 userRouter.post('/', express.json(), createUser);
 
 module.exports = userRouter;
