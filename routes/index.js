2c15f18f2d72d138acea5adacdaa6bb3a2e16e83
Ekaterina Epiphanova <epiph@yandex.ru>
2022-09-04 17:18:13 +0500

    fix: add joi pattern check for avatar in signup request

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().pattern(/^http(s)?:\/\/(www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]{1,256}\.[a-z]{1,6}\b[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
