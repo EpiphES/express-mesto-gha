7d14e4614a973580cbc5cbe89d33046d4fc2230a
Ekaterina Epiphanova <epiph@yandex.ru>
2022-09-04 12:43:27 +0500

    feat: install celebrate for validation of requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const router = require('./routes');
 const errorHandler = require('./middlewares/error');
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/', router);
+app.use(errors());
 app.use(errorHandler);
 
 async function main() {
